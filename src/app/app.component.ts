701461398fb47d1e660bbf792cc5cdf3e1cb7555
MayurBhowad <mayurbhowad1995@gmail.com>
2019-10-05 02:23:14 +0530

    02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'facebook';
+  fname;
+  lname;
+  emailphone;
+  reemailphone;
+  password;
+  gender;
+  month;
+  day;
+  year;
+
+  addUser(
+    fname,
+    lname,
+    emailphone,
+    reemailphone,
+    password,
+    gender,
+    month,
+    day,
+    year
+  ) {
+    this.fname = fname;
+    this.lname = lname;
+    this.emailphone = emailphone;
+    this.reemailphone = reemailphone;
+    this.password = password;
+    this.gender = gender;
+    this.month = month;
+    this.day = day;
+    this.year = year;
+
+    console.log(fname);
+    console.log(lname);
+    console.log(emailphone);
+    console.log(reemailphone);
+    console.log(password);
+    console.log(gender);
+    console.log(`${day}/${month}/${year}`);
+  }
 }
